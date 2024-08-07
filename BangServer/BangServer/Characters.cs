8eb08e74cb8a2feb3a551aa01c1963f7840f52e5
Dae-Hyeon Han <gkseogus93@gmail.com>
2024-08-07 20:32:03 +0900

    Add > Character Class

--- BangServer/BangServer/Characters.cs
+++ BangServer/BangServer/Characters.cs
@@ -10,6 +10,94 @@ namespace BangServer
     {
         string name;        // 캐릭터 이름
         int life;           // 기본 목숨
-        string coments;     // 캐릭터에 대한 설명
+        //string coments;     // 캐릭터에 대한 설명. 클라에서 처리
+    }
+
+    public enum Job
+    {
+        SHERIFF = 0,
+        VICE = 1,
+        OUTLAW = 2,
+        RENEGADE = 3
+    }
+
+    class Willy_The_Kid : Characters
+    {
+        //this.name = 4;
+    }
+
+    class Clamity_Janet : Characters
+    {
+
+    }
+
+    class Kit_Carlson : Characters
+    {
+
+    }
+
+    class Bart_Cassidy : Characters
+    {
+
+    }
+
+    class Sid_Ketchum : Characters
+    {
+
+    }
+
+    class Lucky_Duke : Characters
+    {
+
+    }
+
+    class Jourdonnais : Characters
+    {
+
+    }
+
+    class Black_Jack : Characters
+    {
+
+    }
+
+    class Vulture_Sam : Characters
+    {
+
+    }
+
+    class Jesse_Jones : Characters
+    {
+
+    }
+
+    class Suzy_Lafayette : Characters
+    {
+
+    }
+
+    class Pedro_Ramirez : Characters
+    {
+
+    }
+
+    class Slab_The_Killer : Characters
+    {
+
+    }
+
+    class Rose_Doolan : Characters
+    {
+
+    }
+
+    class Paul_Regret : Characters
+    {
+
+    }
+
+    class El_Gringo: Characters
+    {
+
     }
 }
---
+++ BangServer/BangServer/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BangServer
+{
+    using FreeNet;
+
+    class Player
+    {
+        CGameUser owner;
+        public byte player_index { get; private set; }
+
+        public Player(CGameUser user, byte player_index)
+        {
+            this.owner = user;
+            this.player_index = player_index;
+        }
+
+        public void send(CPacket msg)
+        {
+            this.owner.send(msg);
+            CPacket.destroy(msg);
+        }
+    }
+}
